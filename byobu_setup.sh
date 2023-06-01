4810cd8833260676d1c9bb469a9c785dc9424860
Paul Artushkov <pavtiger@gmail.com>
2023-06-01 03:16:19 +0300

    Add zsh as default shell for byobu startup

--- byobu_setup.sh
+++ byobu_setup.sh
@@ -4,39 +4,46 @@ byobu new-session -d -s pavtiger
 
 # htop
 byobu rename-window -t 0 htop
+byobu send-keys "zsh" C-m
 byobu send-keys "cd" C-m
 byobu send-keys "clear" C-m
 byobu send-keys "htop" C-m
 
 # nvtop
 byobu new-window -n nvtop
+byobu send-keys "zsh" C-m
 byobu send-keys "cd" C-m
 byobu send-keys "clear" C-m
 byobu send-keys "nvtop" C-m
 
 # docker
 byobu new-window -n docker
+byobu send-keys "zsh" C-m
 byobu send-keys "cd" C-m
 byobu send-keys "clear" C-m
 byobu send-keys "docker ps -a" C-m
 
 # scripts
 byobu new-window -n scripts
+byobu send-keys "zsh" C-m
 byobu send-keys "cd" C-m
 byobu send-keys "clear" C-m
 
 # master
 byobu new-window -n master
+byobu send-keys "zsh" C-m
 byobu send-keys "cd /home/pavtiger/4tb/automated/photogrammetry/supervision-server" C-m
 byobu send-keys "clear" C-m
 
 # client
 byobu new-window -n client
+byobu send-keys "zsh" C-m
 byobu send-keys "cd /home/pavtiger/4tb/automated/photogrammetry/process-client" C-m
 byobu send-keys "clear" C-m
 
 # ls
 byobu new-window -n ls
+byobu send-keys "zsh" C-m
 byobu send-keys "cd" C-m
 byobu send-keys "clear" C-m
 
